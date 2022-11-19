d12476032bda819924efca711a4bec115aec71f8
reneald <reneald@protonmail.com>
2022-11-19 13:17:57 +0100

    spring initializr

---
+++ src/main/java/be/leanderonline/cscg/CscgApplication.java
@@ -0,0 +1,13 @@
+package be.leanderonline.cscg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CscgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CscgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/leanderonline/cscg/CscgApplicationTests.java
@@ -0,0 +1,13 @@
+package be.leanderonline.cscg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CscgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
