dd177b3b8da1785d9433108b699017859b2b4044
reneald <reneald@protonmail.com>
2022-11-20 14:22:57 +0100

    rename Stats to Statistics

---
+++ src/main/java/be/leanderonline/cscg/model/stats/Statistics.java
@@ -0,0 +1,4 @@
+package be.leanderonline.cscg.model.stats;
+
+public record Statistics(Statistic might, Statistic speed, Statistic intellect) {
+}
--- src/main/java/be/leanderonline/cscg/model/stats/Stats.java
+++
@@ -1,4 +0,0 @@
-package be.leanderonline.cscg.model.stats;
-
-public record Stats (Statistic might, Statistic speed, Statistic intellect) {
-}
