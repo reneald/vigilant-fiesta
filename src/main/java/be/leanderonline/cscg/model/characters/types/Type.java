2144b22a2e56bc61c10592ab03571b2f1815213a
Leander Van Reeth (100) <leander.vanreeth@cm.be>
2022-11-24 14:47:15 +0100

    model building
    * define constants for type ids
    * define constants for descriptor ids (and create Descriptor class)

--- src/main/java/be/leanderonline/cscg/model/characters/types/Type.java
+++ src/main/java/be/leanderonline/cscg/model/characters/types/Type.java
@@ -3,5 +3,9 @@ package be.leanderonline.cscg.model.characters.types;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("types")
-public record Type(String name, int might, int speed, int intellect, int additionalPoints) {
+public record Type(int id, String name, int might, int speed, int intellect, int additionalPoints) {
+    public static final int WARRIOR = 1;
+    public static final int ADEPT = 2;
+    public static final int EXPLORER = 3;
+    public static final int SPEAKER = 4;
 }
